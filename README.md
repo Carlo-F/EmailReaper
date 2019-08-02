fd7d5265cb31f3a5a186689ddd97e86b08a76460
Carlo-F <carlofeniello@hotmail.it>
2019-08-02 14:11:30 +0200

    Added README file

---
+++ README.md
@@ -0,0 +1,1 @@
+EMAILREAPER is a single-class PHP library to extract emails from webpage or plain text file.
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 require_once "emailReaper.php";
 
-$url = 'http://www.unplitoscana.it/PROLOCO/Provin/prolocgr.htm';
+$url = 'url-to-search-for-emails';
 
 $reaper = new carloF\EmailReaper($url);
 
